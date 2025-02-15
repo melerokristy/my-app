c8370fc922f698928ed8d932495c74d2b8ea5eee
melerokristy <melero.kristy@gmail.com>
2025-02-15 16:51:27 -0600

    updated file name

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,7 +1,7 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherForecastDay(props) {
+export default function WeatherForecastPreview(props) {
   function maxTemperature() {
     let temperature = Math.round(props.data.temp.max);
     return `${temperature}°`;
@@ -22,9 +22,9 @@ export default function WeatherForecastDay(props) {
   }
 
   return (
-    <div>
-      <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+    <div className="WeatherForecastPreview">
+      <div className="forecast-time">{day()}</div>
+      <WeatherIcon code={props.data.condition.icon} size={38} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
